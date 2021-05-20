8067e2d2339e7012457fa8ab41646ca819ec7764
PatrickTelfer <patrickwktelfer@gmail.com>
2021-05-20 19:11:38 -0400

    display timer on prompt screen

--- client/src/Components/PromptInput/PromptInput.js
+++ client/src/Components/PromptInput/PromptInput.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { Container, FullWidthContainer } from "../Common/Container";
 import { Input } from "../Common/Input";
@@ -9,17 +9,18 @@ import { SocketContext } from "../../Context/socket";
 
 const PromptInput = () => {
   const socket = useContext(SocketContext);
+  const [second, setSecond] = useState(10);
   useEffect(() => {
     if (socket) {
       socket.on("timerUpdate", (second) => {
-        console.log(second);
+        setSecond(second);
       });
     }
   }, [socket]);
   return (
     <FullWidthContainer>
       <Container>
-        <Title>Enter a drawing prompt</Title>
+        <Title>Enter a drawing prompt ⏳ {second} ⏳</Title>
         <Spinning src={thinking} alt="loading..." />
         <StyledInput placeholder="Input a suggestion!"></StyledInput>
         <Button>Submit</Button>
