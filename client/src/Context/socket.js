0974bec04e1eefa7f66c412545760ee6687bf549
PatrickTelfer <patrickwktelfer@gmail.com>
2021-06-12 18:30:46 -0400

    new prod build

--- client/src/Context/socket.js
+++ client/src/Context/socket.js
@@ -1,5 +1,7 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-export const socket = socketio.connect("http://localhost:3000");
+export const socket = socketio.connect(
+  "https://fathomless-chamber-92996.herokuapp.com/"
+);
 export const SocketContext = React.createContext();
