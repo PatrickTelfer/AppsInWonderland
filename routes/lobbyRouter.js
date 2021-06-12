f4d206ef45af2a2146386688134066485e0bee98
PatrickTelfer <patrickwktelfer@gmail.com>
2021-06-12 18:40:38 -0400

    update error logging

--- routes/lobbyRouter.js
+++ routes/lobbyRouter.js
@@ -2,27 +2,30 @@ var express = require("express");
 var router = express.Router();
 var LobbyService = require("../services/lobbyService");
 
-// Create a Lobby
-router.post("/", function (req, res, next) {
-  const hostName = req.body.name;
-  const code = LobbyService.createLobby(hostName);
-  res.json({ code: code });
-});
-
 // Get a lobby
 router.get("/:id", function (req, res, next) {
   const id = req.params.id;
   if (id === undefined) {
     console.log("id was undefined");
-    res.status(404);
+    res.status(404).json({
+      error: "id was undefined",
+    });
   }
   const lobby = LobbyService.getLobbyById(id);
   if (lobby === null) {
-    console.log("lobby was null ", id);
-    res.status(404).json({});
+    res.status(404).json({
+      error: "lobby was null",
+    });
   } else {
     res.status(200).json(lobby);
   }
+});
+
+// Create a Lobby
+router.post("/", function (req, res, next) {
+  const hostName = req.body.name;
+  const code = LobbyService.createLobby(hostName);
+  res.json({ code: code });
 });
 
 module.exports = router;
