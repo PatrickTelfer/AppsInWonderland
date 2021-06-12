6cb5f383de9685eb2f49eef7a712af3c5cbe674b
PatrickTelfer <patrickwktelfer@gmail.com>
2021-06-12 18:51:33 -0400

    update error logging

--- app.js
+++ app.js
@@ -21,23 +21,24 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api/lobby", lobbyRouter);
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
