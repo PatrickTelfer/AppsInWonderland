4e41115136efddd0948655109968dcd975fabf1e
Patrick Telfer <patrick.telfer@uwaterloo.ca>
2023-01-28 19:54:52 -0500

    new build

--- client/src/Context/socket.js
+++ client/src/Context/socket.js
@@ -1,7 +1,11 @@
 import socketio from "socket.io-client";
 import React from "react";
 
+// export const socket = socketio.connect(
+//   "https://appsinwonderland.herokuapp.com/"
+// );
+
 export const socket = socketio.connect(
-  "https://appsinwonderland.herokuapp.com/"
+  "http://localhost:3001/"
 );
 export const SocketContext = React.createContext();
